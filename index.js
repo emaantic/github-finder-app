785af2bf78f146f2b47ccb9d856575833f1eae6a
Ema Antić <emaantic196@gmail.com>
2023-06-17 21:11:56 +0200

    update

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const searchBtn = document.getElementById('search');
 
 
 //arrowfunction / ES6
-const getUser = (username) => {
+const getUser = async (username) => {
+    try{
     const url =  'https://api.github.com/users/${username}';
     console.log(url);
-}
+    const response = await (await (fetch (url))).json();
 
+}catch (err) {
+    
+    console.log(err.toString());
+    errorUsername.innerHTML = 'Something is wrong';
+
+}
+}
 const onSearch = () => {
     console.log('Button clicked');
     const errorUsername = document.getElementById('error');

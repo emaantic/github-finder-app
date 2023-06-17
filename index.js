a4225b091df766975d252d5f44d767b5ad84055b
Ema Antić <emaantic196@gmail.com>
2023-06-17 20:57:22 +0200

    add index.js

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const searchBtn = document.getElementById('search');
 
 
 //arrowfunction / ES6
+const getUser = (username) => {
+    const url =  'https://api.github.com/users/${username}';
+    console.log(url);
+}
 
 const onSearch = () => {
     console.log('Button clicked');
+    const errorUsername = document.getElementById('error');
+    document.location.reload();
+
+    if (username.value === ''){
+        console.log('please enter username');
+        errorUsername.insertAdjacentHTML("afterend",'Please enter username');
+    }else {
+        
+        //to do fetch user
+
+    }
 }
 
 searchBtn.addEventListener('click',onSearch);

7f7b9b290ee0b7a63c0e54a4ce03f3b62ae0d084
Ema Antić <emaantic196@gmail.com>
2023-06-18 04:54:21 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,18 +1,90 @@
 const username = document.getElementById('username'); //finds element*(input) by id  in this case username
 const searchBtn = document.getElementById('search'); //finds element*(button) by id  in this case search
 const errorUsername = document.getElementById('error');
+const results = document.getElementById('results');
+
+
+const numberToMonth = (number) => {
+    const calendar=[
+        'January','February','March','April','May','June','July','August','September','October','November','December'
+    ];
+
+    if(number >=1 && number<=12){
+        return calendar[number - 1];
+    }else{
+        return 'Invalid month';
+    }
+
+}
 
 //arrowfunction - ES6 
 const getUser = async (username) => {
     try{
-    const url =  `https://api.gitub.com/users/${username}`;
-    console.log(url);
+    const url =  `https://api.github.com/users/${username}`;
+    console.log(url)
     const response = await (await (fetch (url))).json();
-    if (response.message === 'Not Found'){
-        console.log(err.toString()); 
+        if (response.message === 'Not Found'){
+        
         errorUsername.insertAdjacentHTML("beforeend",'Invalid username');
 
-    }
+        }else {
+        console.log(response)
+        console.log(response.created_at)
+        
+        //ternarni operator
+        const name = response.name ? response.name: "Not Provided";
+        
+         /*
+        if(response.name){
+            name = response.name;
+        }else{
+            name = 'Not provided';
+        }*/ 
+        
+        const username = response.login;
+        //const date_created = response.created_at;
+        const repositories = response.public_repos;
+        const followers = response.followers;
+        const following = response.following;
+        const company = response.company ? response.company : "Not Provided";
+        const bio = response.bio ? response.bio : "No Bio";
+        const email = response.email ? response.email : "No Email";
+        const location = response.location ? response.location : "No Location";
+        const blog = response.blog ? response.blog : "No blog";
+
+        //date
+        //split function at ['T']
+
+        const date_created = response.created_at.split('T')[0];
+        //takes first element of array which is year-month-day
+
+        console.log(date_created)
+
+        const year=date_created.split('-')[0];
+        let month=date_created.split('-')[1];
+        const day=date_created.split('-')[2];
+
+        month=numberToMonth(Number(month));
+
+       
+        //showing the data
+
+        const card= `
+            <h2>Results:</h2>
+            <article class="card">
+                <div>
+                    
+                    <img class="avatar" src="https://github.com/${username}.png">
+                    <div>
+                    <img src="./github-finder-app/Icons/🦆 icon _clock_.png" alt="">
+                    <p>Joined ${day}. ${month} ${year}</p>    
+                    </div>
+                </div>                
+            </article>
+        
+        `
+        results.innerHTML = card;
+        }
 
     }catch (err) {
 
@@ -33,9 +105,6 @@ const onSearch = () => {
         console.log('please enter username');
         errorUsername.insertAdjacentHTML("beforeend",'Please enter username');
     }else {
-
-        
-
         //to do fetch user
         console.log(username.value);
         getUser(username.value);
